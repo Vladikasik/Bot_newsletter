307ec32f552093a2ebab3f4c64727862ae4a0e6e
Vlad <vsakayafignavlad@gmail.com>
2021-01-15 04:40:14 +0300

    Finding title in ria
    not done

--- parser_news.py
+++ parser_news.py
@@ -1,5 +1,6 @@
 import requests 
 from config import *
+from bs4 import BeautifulSoup
 
 class RiaNewsApi():
 
@@ -11,6 +12,12 @@ class RiaNewsApi():
 
 	def test(self):
 		req = requests.get(self.link)
+		soup = BeautifulSoup(req.text, "html.parser")
+		result = soup.findAll('div', {"class": "list-item"})
+		for page in result:
+			a = page.findAll('div', {"class": "list-item__content"})
+			b = a[0].findAll('title')
+			print(a)
 		
 
 class WorldWideApi():
@@ -18,7 +25,20 @@ class WorldWideApi():
 	def __init__(self):
 		self.link = f'http://newsapi.org/v2/top-headlines?country=ru&apiKey={worldwide_api_key}'
 
+	def start_parse(self):
+		pass
+
+	def test(self):
+		print('test')
+		req = requests.get(self.link)
+		
 class MeduzaNewsApi():
 
 	def __init__(self):
 		import meduza
+
+	def start_parse(self):
+		pass
+
+	def test(self):
+		pass
