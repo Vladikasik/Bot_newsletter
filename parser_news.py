5f1d4337e36d07b1a0b207325483e1abee2f5a7a
Vladislav Ainshein <vsakayafignavlad@gmail.com>
2021-01-15 06:41:19 +0300

    modern images

--- parser_news.py
+++ parser_news.py
@@ -1,6 +1,7 @@
 import requests 
 from config import *
 from bs4 import BeautifulSoup
+import time
 
 class RiaNewsApi():
 
@@ -14,10 +15,35 @@ class RiaNewsApi():
 		req = requests.get(self.link)
 		soup = BeautifulSoup(req.text, "html.parser")
 		result = soup.findAll('div', {"class": "list-item"})
-		for page in result:
-			a = page.find('div', {"class": "list-item__content"})
-			b = a.find('a', {'class': 'list-item__title color-font-hover-only'})
-			print(b.text)
+		for state_block in result:
+			print(1)
+			state = state_block.find('div', {"class": "list-item__content"})
+			title = state.find('a', {'class': 'list-item__title color-font-hover-only'})
+			picture = state.find('picture').find('img')
+
+			try:
+				self.write_state(title.text, picture)
+			except Exception as ex:
+				print('waiting')
+				time.sleep(3)
+
+	def write_state(self, title, picture):
+		from telegraph import Telegraph
+
+		telegraph = Telegraph()
+
+		telegraph.create_account(short_name='1337')
+
+		response = telegraph.create_page(
+		    str(title),
+		    html_content=str(picture)
+		)
+		
+
+		print('https://telegra.ph/{}'.format(response['path']))
+
+	def get_state_text(self, url):
+		pass
 		
 
 class WorldWideApi():
