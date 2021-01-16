b8b99ce1593c25d72362c333b2278c2f5375d895
Vlad <vsakayafignavlad@gmail.com>
2021-01-16 16:42:48 +0300

    'bitnews''

--- parser_news.py
+++ parser_news.py
@@ -20,14 +20,18 @@ class RiaNewsApi():
 			state = state_block.find('div', {"class": "list-item__content"})
 			title = state.find('a', {'class': 'list-item__title color-font-hover-only'})
 			picture = state.find('picture').find('img')
+			try:
+				article = self.get_state_text(title['href'])
+			except Exception as e:
+				article = e
 
 			try:
-				self.write_state(title.text, picture)
+				self.write_state(title.text, article, picture)
 			except Exception as ex:
-				print('waiting')
+				print('waiting', ex)
 				time.sleep(3)
 
-	def write_state(self, title, picture):
+	def write_state(self, title, text_state, picture):
 		from telegraph import Telegraph
 
 		telegraph = Telegraph()
@@ -36,14 +40,31 @@ class RiaNewsApi():
 
 		response = telegraph.create_page(
 		    str(title),
-		    html_content=str(picture)
+		    html_content= str(picture) + str(text_state)
 		)
 		
 
 		print('https://telegra.ph/{}'.format(response['path']))
 
 	def get_state_text(self, url):
-		pass
+
+		req = requests.get(url)
+
+		reuslt_html = ''
+
+		soup = BeautifulSoup(req.text, "html.parser")
+		flag_article = False
+		for paragrah in soup.findAll('div', {"class": "article__block"}):
+			if paragrah['data-type'] == 'text':
+				if paragrah['data-type'] == 'article' and flag_article is False:
+					flag_article == True
+					print()
+				if paragrah['data-type'] == 'article' and flag_article:
+					break
+				reuslt_html += str(paragrah.text) + '\n'
+				# print(paragrah.text)
+		return reuslt_html
+
 		
 
 class WorldWideApi():
